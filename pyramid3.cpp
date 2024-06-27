4e9959b69b6df70da23f7947d0f0321e11f9d471
Sanjeevan76 <studyyou40@gmail.com>
2024-06-27 08:06:48 +0530

    180 reverse pyramid

---
+++ pyramid3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    int n,i,j;
+    cout<<"enter number of level";
+    cin>>n;
+
+   for(i=n;i>=1;i--)
+   {
+    for(j=1;j<=i;j++)
+    {
+        cout<<"*";
+    }
+
+    cout<<endl;
+   }
+
+    return 0;
+}
