b15d4b47d00aee60c94d66929a6a22ae43ccc1a3
Sanjeevan76 <studyyou40@gmail.com>
2024-06-27 19:58:04 +0530

    number pyramid

---
+++ pyramidL2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"enter number of level";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(j=1;j<=i;j++)
+        {
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
