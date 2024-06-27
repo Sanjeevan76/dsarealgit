c9ca275ff2b2bc5cdd53731649df1812903f9bfa
Sanjeevan76 <studyyou40@gmail.com>
2024-06-27 19:47:52 +0530

    divert star diagram

---
+++ divertL2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cout<<"enter number of level";
+    cin>>n;
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            cout<<" ";
+        }
+        for(j=1;j<=n;j++)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
