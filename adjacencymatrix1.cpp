afe47e1289e914c367e3ae80ca279a06d795334a
Sanjeevan76 <studyyou40@gmail.com>
2025-07-17 10:45:23 +0530

    unidirect grap

---
+++ adjacencymatrix1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int vertex,edge;
+    cin>>vertex>>edge;
+
+    vector<vector <int> >adj(vertex,vector<int> (vertex,0));
+
+    int u,v;
+
+    for(int i=0;i<edge;i++)
+    {
+       cin>>u>>v;
+       adj[u][v]=1;
+       adj[v][u]=1;
+    }
+
+    for(int j=0;j<vertex;j++)
+    {
+        for(int k=0;k<edge;k++)
+        {
+            cout<<adj[j][k]<<" ";
+        }
+        cout<<endl;
+    }
+}
