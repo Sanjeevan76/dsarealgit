ade2b3ffe3d9b079c759d0831dc38ed24ddaed65
Sanjeevan76 <studyyou40@gmail.com>
2025-07-19 11:22:21 +0530

    traversal of tree

---
+++ binarytree.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node* left;
+    node* right;
+    
+    node(int val)
+    {
+        data=val;
+        left=right=NULL;
+    }
+};
+
+static int idx=-1;
+node* build_tree(vector<int> tree)
+{
+    idx++;
+    if(tree[idx]==-1)
+    {
+        return NULL;
+
+    } 
+
+    node* root=new node(tree[idx]);
+    root->left=build_tree(tree);
+    root->right=build_tree(tree);
+
+    return root;
+}
+
+void preorder(node * root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(node* root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+
+   
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+void postorder(node* root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+
+   
+    postorder(root->left);
+   
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
+
+int main()
+{
+    vector<int> tree={1,2,-1,-1,3,4,-1,-1,5,-1,-1};
+    
+    node* root=build_tree(tree);
+    // cout<<root->data;
+    cout<<"preorder"<<endl;
+    preorder(root);
+    cout<<endl;
+    cout<<"inorder"<<endl;
+    inorder(root);
+    cout<<endl;
+    cout<<"postorder"<<endl;
+    postorder(root);
+
+}
